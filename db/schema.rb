94649edf32da6f56ba3e035ce0c81b50166292f3
Breno Thales <breno.thales@gmail.com>
2016-12-16 16:25:01 -0200

    preparando para fazer deploy no heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227171810) do
+ActiveRecord::Schema.define(version: 20161216182059) do
 
   create_table "contatos", force: :cascade do |t|
     t.string   "nome"
